1dd7469eb42e9d58cf66869f1a0ac16489d46b66
Yoan <yoan16908@gmail.com>
2025-12-06 10:06:45 -0500

    Basic Initialization

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, {json} from 'express'
+import { corsMiddleware } from './middlewares/cors.js'
+
+const app = express()
+
+const PORT = process.env.PORT || '3000'
+
+app.use(json())
+app.use(corsMiddleware())
+app.get('/get',(_req,res)=>{
+  return res.json('Wowwwwww')
+})
+
+app.listen(PORT,()=>{
+  console.log(`Server listening on http://localhost:${PORT}`)
+})
+
---
+++ src/middlewares/cors.ts
@@ -0,0 +1,16 @@
+import cors from 'cors'
+
+const ACCEPTED_ORIGINS = [
+  'http://localhost:3000',
+  'http://localhost:1234',
+]
+
+export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
+  origin: (origin, callback) => {
+    if (!origin || acceptedOrigins.includes(origin)) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+})
